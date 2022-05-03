0b3efe1a052a90d63741209bb8a80565f39f4b0a
Zsofia Koma <komazsofi@gmail.com>
2022-05-03 16:08:26 +0200

    add grouping

--- fielddata_process/Step2_novana_descstat.R
+++ fielddata_process/Step2_novana_descstat.R
@@ -38,6 +38,25 @@ names(data_plot_forshp_biow) <- c("AktID","Year","Habitat","HabitatID","SpRichne
 data_plot_forshp_biow_df=data_plot_forshp_biow@data
 raster::shapefile(data_plot_forshp_biow,"data_plot_forshp_biow_utm",overwrite=TRUE)
 
+# add aggregated groups
+
+data_plot_forshp_biow_df$HabGroup=NA
+
+data_plot_forshp_biow_df$HabGroup[data_plot_forshp_biow_df$HabitatID == 1210 |data_plot_forshp_biow_df$HabitatID == 1220 |data_plot_forshp_biow_df$HabitatID == 1230 |
+                                     data_plot_forshp_biow_df$HabitatID == 2110 |data_plot_forshp_biow_df$HabitatID == 2120 |data_plot_forshp_biow_df$HabitatID == 2130
+                                   |data_plot_forshp_biow_df$HabitatID == 2140 | data_plot_forshp_biow_df$HabitatID == 2160 |data_plot_forshp_biow_df$HabitatID == 2170
+                                   |data_plot_forshp_biow_df$HabitatID == 2190 |data_plot_forshp_biow_df$HabitatID == 2250 |data_plot_forshp_biow_df$HabitatID == 2310
+                                   |data_plot_forshp_biow_df$HabitatID == 2330 | data_plot_forshp_biow_df$HabitatID == 4030 |data_plot_forshp_biow_df$HabitatID == 5130
+                                   |data_plot_forshp_biow_df$HabitatID == 6120 |data_plot_forshp_biow_df$HabitatID == 6210 |data_plot_forshp_biow_df$HabitatID == 6230
+                                   |data_plot_forshp_biow_df$HabitatID == 8220]="Nature dry"
+
+data_plot_forshp_biow_df$HabGroup[data_plot_forshp_biow_df$HabitatID == 1310 |data_plot_forshp_biow_df$HabitatID == 1320 |data_plot_forshp_biow_df$HabitatID == 1330 |
+                                    data_plot_forshp_biow_df$HabitatID == 1340 |data_plot_forshp_biow_df$HabitatID == 4010 |data_plot_forshp_biow_df$HabitatID == 6410
+                                  |data_plot_forshp_biow_df$HabitatID == 7120 | data_plot_forshp_biow_df$HabitatID == 7140 |data_plot_forshp_biow_df$HabitatID == 7150
+                                  |data_plot_forshp_biow_df$HabitatID == 7210 |data_plot_forshp_biow_df$HabitatID == 7220 |data_plot_forshp_biow_df$HabitatID == 7230]="Nature wet"
+
+data_plot_forshp_biow_df$HabGroup[data_plot_forshp_biow_df$HabitatID == 9100 ]="Forest"
+
 # for single year: how many plots per habitat per region
 
 nofplots_perhabreg_yearly=data_plot_forshp_biow_df %>% 
@@ -57,6 +76,24 @@ write.xlsx(nofplots_perhabreg_yearly, file = "nofplots.xlsx",
 
 write.xlsx(nofplots_perhab_yearly, file = "nofplots.xlsx",
            sheetName = "perhab", append = TRUE,showNA=FALSE)
+
+nofplots_perhabreg_yearly_agr=data_plot_forshp_biow_df %>% 
+  group_by(HabGroup,Region, Year) %>%
+  summarize(Count=n()) %>%
+  spread(Year, Count) %>% 
+  ungroup()
+
+nofplots_perhab_yearly_agr=data_plot_forshp_biow_df %>% 
+  group_by(HabGroup, Year) %>%
+  summarize(Count=n()) %>%
+  spread(Year, Count) %>% 
+  ungroup()
+
+write.xlsx(nofplots_perhabreg_yearly_agr, file = "nofplots_aggr.xlsx",
+           sheetName = "perhabperreg_aggr", append = FALSE,showNA=FALSE)
+
+write.xlsx(nofplots_perhab_yearly_agr, file = "nofplots_aggr.xlsx",
+           sheetName = "perhab_aggr", append = TRUE,showNA=FALSE)
 
 # spatial distribution
 
@@ -88,7 +125,3 @@ for (i in habitats){
   
 }
 
-ggplot()+geom_sf(data = biowide_dk_sf)+
-  geom_sf(data = data_plot_forshp_biow_sf[data_plot_forshp_biow_sf$Habitat=="Hede",], aes(color = Habitat),size=2)+
-  facet_wrap(facets = vars(Year))+
-  theme_bw()
