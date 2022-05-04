1cde4981e5e4d20e17fa62220c94b574a943fa0f
Zsofia Viktoria Koma <au700510@uni.au.dk>
2022-05-04 16:28:42 +0200

    intersection whole

---
+++ lidar_process/Step2_intersection.R
@@ -0,0 +1,29 @@
+library(rgdal)
+library(raster)
+
+setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/FeasStudy/processing/field/novana/NOVANAAndP3_tozsofia/")
+
+# Load list of EcoDes-DK15 descriptors
+raster_list <- shell("dir /b /s O:\\Nat_Sustain-proj\\_user\\ZsofiaKoma_au700510\\FeasStudy\\processing\\lidar\\dhm2015\\*.vrt",
+                     intern = T) 
+raster_list <- gsub("\\\\", "/", raster_list)
+
+lidar_metrics=stack(raster_list)
+
+# read in the plot database
+
+plot_db=read.csv("plot_db_welberg.csv")
+plot_db_2016=plot_db[plot_db$Year==2016,]
+
+# make a shapefile
+
+data_plot_forshp=plot_db_2016
+
+coordinates(data_plot_forshp)=~UTM_X+UTM_Y
+proj4string(data_plot_forshp)<- CRS("+proj=utm +zone=32 +ellps=intl +towgs84=-87,-98,-121,0,0,0,0 +units=m +no_defs ")
+
+# intersect
+
+metrics <- extract(lidar_metrics,data_plot_forshp)
+
+plt_db_wlidar=cbind(data_plot_forshp@data,metrics)
