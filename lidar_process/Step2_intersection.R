e43f0c4dff2b7e7927571b596cb9680319455bb8
Zsofia Viktoria Koma <au700510@uni.au.dk>
2022-05-05 12:47:18 +0200

    modified intersect for height meas plot

--- lidar_process/Step2_intersection.R
+++ lidar_process/Step2_intersection.R
@@ -1,7 +1,8 @@
 library(rgdal)
 library(raster)
 
-setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/FeasStudy/processing/field/novana/NOVANAAndP3_tozsofia/")
+#setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/FeasStudy/processing/field/novana/NOVANAAndP3_tozsofia/")
+setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/FeasStudy/processing/field/height_calc/")
 
 # Load list of EcoDes-DK15 descriptors
 raster_list <- shell("dir /b /s O:\\Nat_Sustain-proj\\_user\\ZsofiaKoma_au700510\\FeasStudy\\processing\\lidar\\dhm2015\\*.vrt",
@@ -12,14 +13,16 @@ lidar_metrics=stack(raster_list)
 
 # read in the plot database
 
-plot_db=read.csv("plot_db_welberg.csv")
-plot_db_2016=plot_db[plot_db$Year==2016,]
+#plot_db=read.csv("plot_db_welberg.csv")
+#plot_db_2016=plot_db[plot_db$Year==2016,]
+
+plot_db=read.csv("meanheight_plot.csv")
 
 # make a shapefile
 
-data_plot_forshp=plot_db_2016
+data_plot_forshp=plot_db
 
-coordinates(data_plot_forshp)=~UTM_X+UTM_Y
+coordinates(data_plot_forshp)=~X+Y
 proj4string(data_plot_forshp)<- CRS("+proj=utm +zone=32 +ellps=intl +towgs84=-87,-98,-121,0,0,0,0 +units=m +no_defs ")
 
 # intersect
@@ -28,5 +31,4 @@ metrics <- extract(lidar_metrics,data_plot_forshp)
 
 plt_db_wlidar=cbind(data_plot_forshp@data,metrics)
 
-write.csv(metrics,"onlymetrics.csv")
-write.csv(plt_db_wlidar,"plt_db_wlidar.csv")
+write.csv(plt_db_wlidar,"plt_db_height_wlidar.csv")
